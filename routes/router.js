d144a2f07f134a898158174c0640505778e468e3
BLxcwg666 <huixcwg@gmail.com>
2025-03-27 06:33:41 +0800

    feat: process unmatched route

--- routes/router.js
+++ routes/router.js
@@ -7,4 +7,8 @@ router.get('/pastes/:id', getPaste);
 router.post('/add', addPaste);
 router.post('/del', deletePaste);
 
+router.use((req, res) => {
+    res.json({ success: false, msg: "你在找什么喵？" });
+});
+
 module.exports = router;
