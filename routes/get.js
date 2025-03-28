fc44efa0ae525ffd27b996a731cbdf7aa95124b2
BLxcwg666 <huixcwg@gmail.com>
2025-03-29 00:26:37 +0800

    fix: improve burn after reading

--- routes/get.js
+++ routes/get.js
@@ -3,26 +3,34 @@ const router = express.Router();
 const { client } = require('../modules/redisClient');
 
 router.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const { ownerToken } = req.query;
-
-  const paste = await client.get(id);
-  if (!paste) return res.status(404).json({ error: 'Paste not found' });
-
-  const parsedPaste = JSON.parse(paste);
-
-  if (parsedPaste.keeping === 'burn') {
-    if (parsedPaste.ownerToken && ownerToken === parsedPaste.ownerToken) {
-      return res.json(parsedPaste);
+    const { id } = req.params;
+    const { ownerToken } = req.query;
+  
+    const paste = await client.get(id);
+    if (!paste) return res.status(404).json({ error: 'Paste not found' });
+  
+    const parsedPaste = JSON.parse(paste);
+  
+    if (parsedPaste.keeping === 'burn') {
+      if (parsedPaste.ownerToken && ownerToken === parsedPaste.ownerToken) {
+        res.json(parsedPaste);
+        setTimeout(async () => {
+            const currentData = await client.get(id);
+            if (currentData) {
+              const updatedPaste = JSON.parse(currentData);
+              delete updatedPaste.ownerToken;
+              await client.set(id, JSON.stringify(updatedPaste));
+          }
+        }, 0);
+      } else {
+        res.json(parsedPaste);
+        setTimeout(async () => {
+            await client.del(id);
+        }, 0);
+      }
     } else {
-      res.json(parsedPaste);
-      setTimeout(async () => {
-        await client.del(id);
-      }, 0);
+      return res.json(parsedPaste);
     }
-  } else {
-    return res.json(parsedPaste);
-  }
 });
 
 module.exports = router;
